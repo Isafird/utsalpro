18888814cb09159bed0460712b210e1992d86a35
Isafird <111642604+Isafird@users.noreply.github.com>
2022-11-24 11:10:03 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,12 @@
+public class Variabel
+{
+    public static void Variabel(String[] args) {
+        String Salam = "Assalamu'alaikum";
+        String nama = "Muhamad Isa Firdaus";
+        int Tahun_lahir = 2003;
+
+        System.out.println(Salam);
+        System.out.println("Nama Saya : " + Nama);
+        System.out.println("Saya Lahir di Tahun : " + Tahun_lahir);
+    }
+}
